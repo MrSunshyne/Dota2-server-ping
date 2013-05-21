8f9d538855b6c1dd9e690761b886891518126da7
Sandeep Ramgolam <s4ndeep1203@gmail.com>
2013-05-21 13:55:08 +0400

    added country menu

--- Dota2ServerPing.bat
+++ Dota2ServerPing.bat
@@ -9,17 +9,69 @@ echo.
 echo What would you like to do?
 echo.
 echo 1. Ping all the Dota2 Servers.
-echo 2. Watch the world burn.
+echo 2. SE Asia
+echo 3. Russia
+echo 4. Luxembourg
+echo 5. Watch the world burn.
 echo 0. Quit
 echo.
 echo.Written by Mr.Sunshyne for MRU gamers.
 echo.
 set /p choice="So what's it gonna be? : "
 if "%choice%"=="1" goto checktheping
-if "%choice%"=="2" goto burnworld
+if "%choice%"=="2" goto checksea
+if "%choice%"=="3" goto checkrussia
+if "%choice%"=="4" goto checklux
+if "%choice%"=="5" goto burnworld
 if "%choice%"=="0" exit
 echo Invalid choice: %choice%
 echo.
+pause
+cls
+goto start
+
+:checksea
+echo.
+echo -------------------------------------------------------------
+echo Ping Check Starting. Un petit patience.
+echo -------------------------------------------------------------
+echo.
+echo -------------------------------------------------------------
+echo + Old Sea
+echo -------------------------------------------------------------
+ping -n 2 103.28.54.1
+echo.
+echo _____________________________________________________________
+echo.
+echo -------------------------------------------------------------
+echo + New Sea
+echo -------------------------------------------------------------
+ping -n 2 103.10.124.148
+echo.
+echo _____________________________________________________________
+pause
+cls
+goto start
+
+
+:checkrussia
+echo -------------------------------------------------------------
+echo + Stockholm : Russia
+echo -------------------------------------------------------------
+ping -n 2 146.66.156.2
+echo.
+echo _____________________________________________________________
+pause
+cls
+goto start
+
+:checklux
+echo -------------------------------------------------------------
+echo + Luxembourg : Eu West
+echo -------------------------------------------------------------
+ping -n 2 146.66.152.1
+echo.
+echo _____________________________________________________________
 pause
 cls
 goto start
